ef51cecd485ce4cc0684bf8d9904f3ad15974087
joseph <github@afk.pw>
2026-02-19 01:12:48 -0500

    fix: suppress error pages on overlay window

---
+++ src/routes/overlay/+error.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<style>
+		html,
+		body {
+			background: transparent !important;
+			overflow: hidden;
+			pointer-events: none;
+			cursor: none;
+			user-select: none;
+		}
+	</style>
+</svelte:head>
